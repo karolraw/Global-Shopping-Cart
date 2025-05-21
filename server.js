693d0553cc32b423a497f416f7fe2f3b3c3e42ef
karolraw <karolrawiak1502@gmail.com>
2025-05-21 06:37:51 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require("http")
+const fs = require("fs")
+const path = require("path")
+const port = 3000
+
+const server = http.createServer((request, response) => {
+    let file = request.url === "/" ? "index.html": request.url
+    let filePath = path.join(__dirname, file)
+    let ext = path.extname(file)
+
+    let types = {
+        ".html": "text/html",
+        ".css": "text/css",
+        ".js": "text/javascript"
+    }
+
+    fs.readFile(filePath, (error, data) => {
+        if (error) {
+            response.writeHead(404)
+            response.end("File not found")
+        } else {
+            response.writeHead(200, {"Content-Type": types[ext] || "text/plain"})
+            response.end(data)
+        }
+    })
+})
+
+server.listen(port, error => {
+    if (error) {
+        console.log("Something went wrong", error)
+    } else {
+        console.log("Server is listening on port " + port)
+    }
+})
+
+
